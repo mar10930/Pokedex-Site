ee4adec36863d3a5053c7c9b316341b4a479d4e6
mar10930 <mario4education@gmail.com>
2022-05-27 18:21:27 -0700

    Display header and picture for selected pokemon

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const url = "https://pokeapi.co/api/v2/pokemon?limit=151"
+const url = "https://pokeapi.co/api/v2/"
 let pokemonCall = new Array();
+let  pokemonInfo = new Array();
 //Fetch the api call
 
-fetch(url)
+fetch(url+"pokemon?limit=151")
     //If the promise is fulfilled we get the json response
     .then(response => response.json())
     .then(data =>
@@ -11,6 +12,7 @@ fetch(url)
         pokemonCall = data.results;
         console.log(pokemonCall[120]);
         dispPokemon();
+        
     })
     //If the call was not successful display error
     .catch(error => {console.log("Error couldn't retreive pokemon!!!",error);});
@@ -28,7 +30,9 @@ function dispPokemon()
             let li = document.createElement("li");
             li.setAttribute("id","name");
             let strEntry = String("#00" + pokemonNum + " " + pokemonCall[i].name);
-            let buttonTxt = "<button type=\"button\" class=\"pokemon\">"+ strEntry ;
+            let buttonTxt = "<button type=\"button\"" + "id=\"pokemon" +pokemonNum +"\" " 
+            + "class=\"pokemon\"" + "onClick=\"displayPokemon(this.id)\"" + "data-num=" +
+            "\""+pokemonNum+"\">"+ strEntry+"</button>";
             li.innerHTML = buttonTxt;
             ul.appendChild(li);
         }
@@ -38,7 +42,9 @@ function dispPokemon()
             let li = document.createElement("li");
             li.setAttribute("id","name");
             let strEntry = String("#0" + pokemonNum + " " + pokemonCall[i].name);
-            let buttonTxt = "<button type=\"button\" class=\"pokemon\">"+ strEntry;
+            let buttonTxt = "<button type=\"button\"" + "id=\"pokemon" +pokemonNum +"\" " 
+            + "class=\"pokemon\"" + "onClick=\"displayPokemon(this.id)\"" + "data-num=" +
+            "\""+pokemonNum+"\">"+ strEntry+"</button>";
             li.innerHTML = buttonTxt;
             ul.appendChild(li);
         }
@@ -48,12 +54,57 @@ function dispPokemon()
             let li = document.createElement("li");
             li.setAttribute("id","name");
             let strEntry = String("#" + pokemonNum + " " + pokemonCall[i].name);
-            let buttonTxt = "<button type=\"button\" class=\"pokemon\">"+ strEntry;
+            let buttonTxt = "<button type=\"button\"" + "id=\"pokemon" +pokemonNum +"\" " 
+            + "class=\"pokemon\"" + "onClick=\"displayPokemon(this.id)\"" + "data-num=" +
+            "\""+pokemonNum+"\">"+ strEntry+"</button>";
             li.innerHTML = buttonTxt;
-            ul.appendChild(li);
-            
+            ul.appendChild(li);          
         }
         pokemonNum++;
     }
+
+   
 }
+
+function displayPokemon(btnId)
+{
+    let num = parseInt(document.getElementById(btnId).getAttribute("data-num"));
+    let dispNum = "";
+    let pokeName = String(pokemonCall[num-1].name);
+    let displayName = pokeName.charAt(0).toUpperCase() + pokeName.slice(1);
+
+    //Call the api
+    callPokemon(num);
+
+    if(num >=1 && num <= 9)
+    {
+        dispNum = "#00" + num;
+    }
+
+    else if(num >= 10 && num <= 99)
+    {
+        dispNum = "#0" +num;
+    }
+
+    else{
+        dispNum = "#" + num;
+    }
+    document.getElementById("pokeNum").innerHTML = dispNum;
+    document.getElementById("pokemonHeader").innerHTML = displayName;
+}
+
+function callPokemon(num)
+{
+    fetch(url+"pokemon/"+num)
+    .then(response =>response.json())
+    .then(data=>
+    {
+        let spritePoke = data.sprites;
+        console.log(spritePoke.back_default);
+        document.getElementById("pokemonImg").src = spritePoke.front_default;
+    })
+    .catch(error=>{console.log("error could not get sprite",error);});    
+
+}
+
 
